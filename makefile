0fbb1ccc729e418f69302e9864ed93ad0f203fcb
vikingofvalhalla <socialmedia@email.nordtechsystems.com>
2023-11-24 20:52:03 -0500

    Added .gitignore, created webserver for linux, added makefile.

---
+++ makefile
@@ -0,0 +1,29 @@
+#!/usr/bin/env make
+
+SrcDir=src
+IncludeDir=$(SrcDir)
+BuildDir = build
+BuildBinDir = $(BuildDir)/bin
+BuildObjectsDir = $(BuildDir)/objects
+
+CC = g++
+CFlags = -I $(IncludeDir) -g
+
+
+all: $(BuildBinDir)/main
+
+prerequisites:
+	@ mkdir -p $(BuildBinDir)
+	@ mkdir -p $(BuildObjectsDir)
+
+./build/bin/main: \
+		$(BuildObjectsDir)/server_linux.o \
+		$(BuildObjectsDir)/http_tcpServer_linux.o \
+		# $(BuildObjectsDir)/syntax_validation.o | prerequisites
+	@ echo Building $@ from $^
+	@ $(CC) -o $@ $^
+
+
+$(BuildObjectsDir)/%.o: $(SrcDir)/%.cpp | prerequisites
+	@ echo Building $@ from $<
+	@ $(CC) $(CFlags) -c -o $@ $<
---
+++ src/server_linux.cpp
@@ -0,0 +1,25 @@
+/**
+ * http_tcpServer_linux.h
+ * http_tcpServer_linux.cpp -- will hold the code for our actual server
+ * implementation via the TcpServer class.
+ *
+ *
+ * server_linux.cpp -- will have a “main” function through which
+ * will run the server using the TcpServer object.
+ */
+
+#include <iostream>
+
+#include "cpp_webserver/http_tcpServer_linux.hpp"
+
+int main() {
+    using namespace http;
+    TcpServer server = TcpServer("0.0.0.0", 8080);
+    bool serverClosed = server.startListen();
+    std::cout << "Server status: " << serverClosed << std::endl;
+    if (serverClosed) {
+        server.closeServer();
+    }
+
+    return 0;
+}
