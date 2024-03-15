6f1e4d2f199175939d6a66a162dffb7e5c24bae9
vikingofvalhalla <socialmedia@email.nordtechsystems.com>
2024-03-15 13:33:33 -0400

    Added SO_REUSEADDR option to allow starting server back up after sending a request.

--- src/cpp_files/http_server.cpp
+++ src/cpp_files/http_server.cpp
@@ -86,6 +86,13 @@ bool HttpServer::bindSocket() {
     server_address.sin_addr.s_addr = INADDR_ANY;
     server_address.sin_port = htons(std::stoi(port));
 
+    // set SO_REUSEADDR option
+    int opt = 1;
+    if (setsockopt(server_socket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) == -1) {
+        perror("Setsockopt failed");
+        return false;
+    }
+
     if (bind(server_socket, (struct sockaddr *)&server_address, sizeof(server_address)) == -1) {
         perror("Bind failed");
         return false;
