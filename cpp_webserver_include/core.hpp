2a0d266f8d4612350cd343d19061c3d9146a1dbb
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2024-11-04 17:33:36 -0500

    Revised for library and is working now.

--- cpp_webserver_include/core.hpp
+++ cpp_webserver_include/core.hpp
@@ -3,9 +3,9 @@
 #ifndef HTTP_SERVER_HPP
 #define HTTP_SERVER_HPP
 
-#include "src/hpp_files/http_server.hpp"    // Main HTTP server class
+#include "src/hpp_files/server_logging.hpp" // Optional: Logging support
 #include "src/hpp_files/request.hpp"        // Handles HTTP requests
 #include "src/hpp_files/response.hpp"       // Handles HTTP responses
-#include "src/hpp_files/server_logging.hpp" // Optional: Logging support
+#include "src/hpp_files/http_server.hpp"    // Main HTTP server class
 
 #endif // HTTP_SERVER_HPP
