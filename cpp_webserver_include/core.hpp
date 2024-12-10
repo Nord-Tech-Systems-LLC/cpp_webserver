ef3fae016986b46733f89771704de872e5869e23
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2024-12-10 14:59:21 -0500

    Changed hpp_files to header_files.

--- cpp_webserver_include/core.hpp
+++ cpp_webserver_include/core.hpp
@@ -3,9 +3,10 @@
 #ifndef HTTP_SERVER_HPP
 #define HTTP_SERVER_HPP
 
-#include "src/hpp_files/server_logging.hpp" // Optional: Logging support
-#include "src/hpp_files/request.hpp"        // Handles HTTP requests
-#include "src/hpp_files/response.hpp"       // Handles HTTP responses
-#include "src/hpp_files/http_server.hpp"    // Main HTTP server class
+#include "src/header_files/request.hpp"        // Handles HTTP requests
+#include "src/header_files/response.hpp"       // Handles HTTP responses
+#include "src/header_files/server_logging.hpp" // Optional: Logging support
+
+#include "src/header_files/http_server.hpp" // Main HTTP server class
 
 #endif // HTTP_SERVER_HPP
