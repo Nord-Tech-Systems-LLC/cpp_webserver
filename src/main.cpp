a5b08bff0db86c6f6b72752dadfde5f3d52721a4
VikingOfValhalla <tom.wilson@nordtechsystems.com>
2026-04-12 13:14:43 -0400

    Fixed routes.

--- src/main.cpp
+++ src/main.cpp
@@ -19,8 +19,8 @@ class Users {
 
 void Users::getUserListData(Request &req, Response &res) {
     try {
-        int offset = std::stoi(req.query.at(":offset"));
-        int limit = std::stoi(req.query.at(":limit"));
+        int offset = std::stoi(req.query["offset"]);
+        int limit = std::stoi(req.query["limit"]);
 
         res.setStatus(200).setBody("User List Data");
     } catch (const std::invalid_argument &e) {
@@ -38,30 +38,31 @@ void Users::createUser(Request &req, Response &res) {
 
 void Users::getCurrentUser(Request &req, Response &res) {
     try {
-        res.setStatus(200).setBody("Current User!");
+        // res.setHeader("Content-Type", "application/json");
+        res.setStatus(200).setBody(req.body);
     } catch (const std::invalid_argument &e) {
         res.setStatus(400).setBody("Bad Request: " + std::string(e.what()));
     } catch (const std::exception &e) { res.setStatus(500).setBody("Internal Server Error: " + std::string(e.what())); }
 }
 
 void user_controller(HttpServer &server) {
-    server.get("/list_users/:offset/:limit", Users::getUserListData);
-    server.get("/current_user", Users::getCurrentUser);
-    server.post("/create_user", Users::createUser);
+    server.get("/api/list_users", Users::getUserListData);
+    server.post("/api/current_user", Users::getCurrentUser);
+    server.post("/api/create_user", Users::createUser);
 }
 
 int main() {
     HttpServer server("127.0.0.1", "8080");
-    // server.middleware({{"Access-Control-Allow-Origin", "*"}}); // set global header config
     server.use([](Request &req, Response &res, Next next) {
         res.setHeader("Access-Control-Allow-Origin", "*");
         res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-        res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        res.setHeader("Access-Control-Allow-Headers", "Content-Type");
 
         if (req.method == "OPTIONS") {
             res.setStatus(204);
             return;
         }
+        next();
     });
     user_controller(server); // load route controller
     server.printRoutes();
